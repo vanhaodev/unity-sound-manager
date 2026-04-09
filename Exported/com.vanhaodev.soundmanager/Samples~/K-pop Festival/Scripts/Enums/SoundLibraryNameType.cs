e49a210ebf59e6b52593cab0936fdc5dd2f2e072
noednguyen <noednguyen@ntada.vn>
2026-04-09 17:07:06 +0700

    Update sample

---
+++ Exported/com.vanhaodev.soundmanager/Samples~/K-pop Festival/Scripts/Enums/SoundLibraryNameType.cs
@@ -0,0 +1,12 @@
+// Auto-generated enum by SoundManager
+// Do not edit manually. Changes will be overwritten.
+
+namespace vanhaodev.soundmanager.generated
+{
+	public enum SoundLibraryNameType
+	{
+		bell = 0,
+		maintheme1 = 1,
+		maintheme2 = 2,
+	}
+}
